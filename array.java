912821c8cdce109bdc3efecbf5730bd34d98bd04
chenglinbi <clbi@myseneca.ca>
2018-06-24 02:03:50 -0400

    first commit

---
+++ array.java
@@ -0,0 +1,16 @@
+public class DSAArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[5];
+		
+		a[0] = 11;
+		a[1] = 41;
+		a[2] = -8;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
